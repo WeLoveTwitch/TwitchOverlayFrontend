d8621d010c4229f1f6564e0b38c82b9dad83f66d
Devon Zara <hello@devonzara.com>
2015-02-24 02:39:21 -0500

    Add style handling and debug info

--- overlay/services/ComponentsService.js
+++ overlay/services/ComponentsService.js
@@ -1,40 +1,45 @@
-TwitchOverlay.service('Components', [function() {
+TwitchOverlay.service('ComponentsService', [function () {
 
     var components = [];
 
     function find(id) {
-        for(var i = 0; i < components.length; i++) {
-            if(components[i]._id === id) {
+        for (var i = 0; i < components.length; i++) {
+            if (components[i]._id === id) {
                 return components[i];
             }
         }
-        return;
+
+        return false;
     }
 
     return {
-        add: function(component) {
+        add: function (component) {
             components.push(component);
         },
-        fill: function(componentsFromServer) {
+        fill: function (componentsFromServer) {
             components = componentsFromServer;
         },
-        remove: function(id) {
-            components = components.filter(function(component) {
+        remove: function (id) {
+            components = components.filter(function (component) {
                 return component._id !== id;
             });
         },
-        get: function() {
+        get: function () {
             return components;
         },
-        update: function(componentName, eventName, componentId, data) {
-            console.log(componentName, eventName, componentId, data);
+        update: function (componentName, eventName, componentId, data) {
+            console.debug('ComponentsService::update', componentName, eventName, componentId, data);
             var component = find(componentId);
-            if(!component) {
-                console.log('trying to update a non-existing component', componentId);
-                return;
+
+            if (!component) {
+                console.warn('Warning! Trying to update an unknown component: ' + componentName, componentId);
+                return false;
             }
-            for(var key in data) {
-                component[key] = data[key];
+
+            for (var key in data) {
+                if (data.hasOwnProperty(key)) {
+                    component.settings[key] = data[key];
+                }
             }
         }
     }
